8dbfbf4b069b86a67e8df5be44314e3d602d7daa
Tommaso Diotalevi <tommaso.diotalevi@cnaf.infn.it>
2022-07-14 17:27:59 +0000

    Add configuration header file
    This header file contains the Configuration struct, with all the
    parameters initialisation for the Tape API: IP address, port, API uri
    and well_known endpoint.

---
+++ configuration.hpp
@@ -0,0 +1,15 @@
+#ifndef CONFIGURATION_H
+#define CONFIGURATION_H
+#include <string>
+#include <crow.h>
+
+struct Configuration
+{
+  std::string ip_address      = "127.0.0.1"; //localhost
+  std::string base_url        = "http://"+ ip_address + ":" + std::to_string(port);
+  std::string api_uri         = base_url + "/api/v1";
+  std::string well_known_uri  = "/.well-known/wlcg-tape-rest-api";
+  std::uint16_t port          = 8080;
+};
+
+#endif
