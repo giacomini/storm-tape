64c890373d4b93ec1f07e38962d5d030df84b005
Tommaso Diotalevi <tommaso.diotalevi@cnaf.infn.it>
2022-07-20 09:31:16 +0000

    Clean code

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
+#include "configuration.hpp"
 #include "database.hpp"
 #include "tape_api.hpp"
-#include "configuration.hpp"
 
 int main(int, char*[])
 {
   soci::session sql(soci::sqlite3, "storm-tape.db");
   crow::SimpleApp app;
   MockDatabase db;
-  Configuration const& config{};
+  Configuration config{};
+
   create_routes(app, config, db);
   // Configuring server with hostname instead of IP address with Crow?
   app.port(config.port).run();
