5e58348116bda61f60a38658dccc9b972d6829dc
Tommaso Diotalevi <tommaso.diotalevi@cnaf.infn.it>
2022-07-20 09:40:32 +0000

    Fix preprocessor directives and improve formatting

--- configuration.hpp
+++ configuration.hpp
@@ -1,15 +1,15 @@
-#ifndef CONFIGURATION_H
-#define CONFIGURATION_H
-#include <string>
+#ifndef CONFIGURATION_HPP
+#define CONFIGURATION_HPP
 #include <crow.h>
-
+#include <netdb.h>
+#include <string>
 struct Configuration
 {
   std::string hostname = "localhost";
   std::string base_url = "https://" + hostname + ":" + std::to_string(port);
-  std::string api_uri         = base_url + "/api/v1";
-  std::string well_known_uri  = "/.well-known/wlcg-tape-rest-api";
-  std::uint16_t port          = 8080;
+  std::string api_uri  = base_url + "/api/v1";
+  std::string well_known_uri = "/.well-known/wlcg-tape-rest-api";
+  std::uint16_t port         = 8080;
 };
 
 #endif
