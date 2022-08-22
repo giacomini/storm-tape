4f59faf61f8c4761212f1fd93bfede48344db1da
Tommaso Diotalevi <tommaso.diotalevi@cnaf.infn.it>
2022-08-22 12:06:20 +0000

    Add tape responses for different API operations
    This file includes several tape responses, depending on the API
    operation. All the tape responses return a crow response object.
    For the header file, it also includes member variables with corresponding get
    methods.
    General code cleaning and aesthetics.

--- tape_response.cpp
+++ tape_response.cpp
@@ -1,11 +1,61 @@
 #include "tape_response.hpp"
 #include "configuration.hpp"
 
-crow::response TapeResponse::stage(json::object jbody,
-                                   Configuration const& config)
+crow::response storm::TapeResponse::staged(boost::json::object jbody,
+                                           storm::Configuration const& config)
 {
-  crow::response resp{crow::status::CREATED, "json", json::serialize(jbody)};
-  std::string id = jbody.at("requestId").as_string().c_str();
-  resp.add_header("Location", config.api_uri + "/stage/" + id);
+  crow::response resp{crow::status::CREATED, "json",
+                      boost::json::serialize(jbody)};
+  resp.add_header("Location", config.api_uri + "/stage/" + m_id);
   return resp;
+}
+
+crow::response storm::TapeResponse::status(boost::json::object jbody)
+{
+  return crow::response{crow::status::OK, "json", boost::json::serialize(jbody)};
+}
+
+crow::response storm::TapeResponse::cancelled()
+{
+  return crow::response{crow::status::OK};
+}
+
+crow::response storm::TapeResponse::erased()
+{
+  return crow::response{crow::status::OK};
+}
+
+crow::response storm::TapeResponse::released()
+{
+  return crow::response{crow::status::OK};
+}
+
+crow::response storm::TapeResponse::fetched_from_archive(boost::json::array jbody)
+{
+  return crow::response{crow::status::OK, "json", boost::json::serialize(jbody)};
+}
+
+crow::response storm::TapeResponse::bad_request_with_body(boost::json::object jbody)
+{
+  return crow::response(crow::status::BAD_REQUEST, "json", boost::json::serialize(jbody));
+}
+
+crow::response storm::TapeResponse::bad_request()
+{
+  return crow::response(crow::status::BAD_REQUEST);
+}
+
+crow::response storm::TapeResponse::not_found()
+{
+  return crow::response(crow::status::NOT_FOUND);
+}
+
+std::string storm::TapeResponse::getId() const
+{
+  return m_id;
+}
+
+std::vector<std::filesystem::path> storm::TapeResponse::getInvalid() const
+{
+  return m_invalid;
 }
--- tape_response.hpp
+++ tape_response.hpp
@@ -4,15 +4,41 @@
 #include <boost/json.hpp>
 #include <crow.h>
 #include <string>
+#include <filesystem>
 
-namespace json = boost::json;
-
+namespace storm {
 class Configuration;
 
 class TapeResponse
 {
+ private:
+  std::string m_id;
+  std::vector<std::filesystem::path> m_invalid; //
+
  public:
-  static crow::response stage(json::object jbody, Configuration const& config);
+  TapeResponse(std::string const& id, std::vector<std::filesystem::path>& invalid)
+      : m_id(id)
+      , m_invalid(invalid)
+  {}
+  TapeResponse(std::string const& id)
+      : m_id(id)
+  {}
+  TapeResponse() {}
+
+  crow::response staged(boost::json::object jbody, Configuration const& config);
+  crow::response status(boost::json::object jbody);
+  crow::response cancelled();
+  crow::response erased();
+  crow::response released();
+  crow::response fetched_from_archive(boost::json::array jbody);
+  crow::response bad_request_with_body(boost::json::object jbody);
+  static crow::response bad_request();
+  static crow::response not_found();
+
+  std::string getId() const;
+  std::vector<std::filesystem::path> getInvalid() const;
 };
+
+} // namespace storm
 
 #endif
