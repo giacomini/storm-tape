c1e27e115155d5e4d97739ecbd0b2d7893957323
Tommaso Diotalevi <tommaso.diotalevi@cnaf.infn.it>
2022-08-22 11:48:55 +0000

    Replace database object with tapeservice

--- main.cpp
+++ main.cpp
@@ -6,10 +6,8 @@ int main(int, char*[])
 {
   soci::session sql(soci::sqlite3, "storm-tape.db");
   crow::SimpleApp app;
-  MockDatabase db;
-  Configuration config{};
+  storm::TapeService service{db};
 
-  create_routes(app, config, db);
-  // Configuring server with hostname instead of IP address with Crow?
+  create_routes(app, config, service);
   app.port(config.port).run();
 }
