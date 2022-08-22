72581da50c66a2f432643c34c14ca18ede09de8f
Tommaso Diotalevi <tommaso.diotalevi@cnaf.infn.it>
2022-08-22 11:49:04 +0000

    Clean code

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "configuration.hpp"
 #include "database.hpp"
-#include "tape_api.hpp"
+#include "routes.hpp"
+#include "tape_service.hpp"
 
 int main(int, char*[])
 {
   soci::session sql(soci::sqlite3, "storm-tape.db");
   crow::SimpleApp app;
+  storm::MockDatabase db;
+  storm::Configuration config{};
   storm::TapeService service{db};
 
   create_routes(app, config, service);
