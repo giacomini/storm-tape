c6d0f0ec32f15b30e63c6e4b20f9a8f271733590
Tommaso Diotalevi <tommaso.diotalevi@cnaf.infn.it>
2022-09-09 20:19:00 +0000

    Create status response class (.hpp and .cpp files)
    With the creation of this header and source file, the Response class for
    the API "status" operation (a.k.a progress tracking) has been declared
    and defined. Now, all the Response operations are split in different
    files, compared to the previous single "TapeResponse" class, handling
    all the API operations together.

---
+++ status_response.hpp
@@ -0,0 +1,38 @@
+#ifndef STATUS_RESPONSE_HPP
+#define STATUS_RESPONSE_HPP
+
+#include <crow.h>
+#include <string>
+#include <filesystem>
+#include <boost/json.hpp>
+
+namespace storm {
+class Configuration;
+class StageRequest;
+
+class StatusResponse
+{
+ private:
+  std::string m_id;
+  StageRequest const* m_stage;
+
+ public:
+  StatusResponse(std::string const& id, StageRequest const* stage)
+      : m_id(std::move(id))
+      , m_stage(stage)
+  {}
+  StatusResponse(StageRequest const* stage)
+      : m_stage(stage)
+  {}
+
+  crow::response status(boost::json::object const& jbody) const;
+  std::string const& id() const;
+  StageRequest const* stage() const;
+
+  static crow::response bad_request();
+  static crow::response not_found();
+};
+
+} // namespace storm
+
+#endif
