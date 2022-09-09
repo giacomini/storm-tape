6e5a0175784349a6060e6e9cbb544582e1379dd1
Tommaso Diotalevi <tommaso.diotalevi@cnaf.infn.it>
2022-09-09 20:21:04 +0000

    Create cancel response class (.hpp and .cpp files)
    With the creation of this header and source file, the Response class for
    the API "cancel" operation has been declared and defined. Now, all the
    Response operations are split in different files, compared to the
    previous single "TapeResponse" class, handling all the API operations
    together.

---
+++ cancel_response.cpp
@@ -0,0 +1,38 @@
+#include "cancel_response.hpp"
+
+std::string const& storm::CancelResponse::id() const
+{
+  return m_id;
+}
+
+storm::StageRequest const* storm::CancelResponse::stage() const
+{
+  return m_stage;
+}
+
+std::vector<std::filesystem::path> const& storm::CancelResponse::invalid() const
+{
+  return m_invalid;
+}
+
+crow::response
+storm::CancelResponse::bad_request_with_body(boost::json::object jbody)
+{
+  return crow::response(crow::status::BAD_REQUEST, "json",
+                        boost::json::serialize(jbody));
+}
+
+crow::response storm::CancelResponse::bad_request()
+{
+  return crow::response(crow::status::BAD_REQUEST);
+}
+
+crow::response storm::CancelResponse::not_found()
+{
+  return crow::response(crow::status::NOT_FOUND);
+}
+
+crow::response storm::CancelResponse::cancelled()
+{
+  return crow::response{crow::status::OK};
+}
---
+++ cancel_response.hpp
@@ -0,0 +1,43 @@
+#ifndef CANCEL_RESPONSE_HPP
+#define CANCEL_RESPONSE_HPP
+
+#include <boost/json.hpp>
+#include <crow.h>
+#include <filesystem>
+#include <string>
+
+namespace storm {
+class Configuration;
+class StageRequest;
+
+class CancelResponse
+{
+ private:
+  std::string m_id;
+  std::vector<std::filesystem::path> m_invalid;
+  StageRequest const* m_stage;
+
+ public:
+  CancelResponse(StageRequest const* stage)
+      : m_stage(stage)
+  {}
+  CancelResponse(std::string const& id,
+                 std::vector<std::filesystem::path>& invalid)
+      : m_id(std::move(id))
+      , m_invalid(std::move(invalid))
+  {}
+  CancelResponse()
+  {}
+
+  std::string const& id() const;
+  StageRequest const* stage() const;
+  std::vector<std::filesystem::path> const& invalid() const;
+  static crow::response bad_request_with_body(boost::json::object jbody);
+  static crow::response bad_request();
+  static crow::response not_found();
+  static crow::response cancelled();
+};
+
+} // namespace storm
+
+#endif
