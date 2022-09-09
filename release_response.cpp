e5b681b73b308afbbbe419fd58fc50845a9bc1d5
Tommaso Diotalevi <tommaso.diotalevi@cnaf.infn.it>
2022-09-09 20:25:45 +0000

    Create release response class (.hpp and .cpp)
    With the creation of this header and source file, the Response class for
    the API "Release" operation has been declared and defined. Now, all the
    Response operations are split in different files, compared to the
    previous single "TapeResponse" class, handling all the API operations
    together.
    This API operation is still under work in progress, and will probably be
    deprecated due to storm impossiblity to release staged files from cache.

---
+++ release_response.cpp
@@ -0,0 +1,38 @@
+#include "release_response.hpp"
+
+std::string const& storm::ReleaseResponse::id() const
+{
+  return m_id;
+}
+
+storm::StageRequest const* storm::ReleaseResponse::stage() const
+{
+  return m_stage;
+}
+
+std::vector<std::filesystem::path> const& storm::ReleaseResponse::invalid() const
+{
+  return m_invalid;
+}
+
+crow::response
+storm::ReleaseResponse::bad_request_with_body(boost::json::object jbody)
+{
+  return crow::response(crow::status::BAD_REQUEST, "json",
+                        boost::json::serialize(jbody));
+}
+
+crow::response storm::ReleaseResponse::bad_request()
+{
+  return crow::response(crow::status::BAD_REQUEST);
+}
+
+crow::response storm::ReleaseResponse::not_found()
+{
+  return crow::response(crow::status::NOT_FOUND);
+}
+
+crow::response storm::ReleaseResponse::released()
+{
+  return crow::response{crow::status::OK};
+}
