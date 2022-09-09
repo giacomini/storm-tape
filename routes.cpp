e7403e7fb1738e2c07b6e55395249f94a6414fb3
Tommaso Diotalevi <tommaso.diotalevi@cnaf.infn.it>
2022-09-09 21:04:05 +0000

    Synthesize routes endpoints
    This routes file, containing all the crow route endpoints, has been
    synthesized, moving all the operations and IO to the corresponding
    files (tape_service and IO, mainly) and leaving only the basic
    request/response operations.

--- routes.cpp
+++ routes.cpp
@@ -33,30 +33,25 @@ void create_routes(crow::SimpleApp& app, storm::Configuration const& config,
   CROW_ROUTE(app, "/api/v1/stage")
       .methods("POST"_method)([&](crow::request const& req) {
         try {
-          auto files = storm::fromJSONPath(req.body);
-          storm::StageRequest stage{files};
-          auto resp  = service.stage(stage);
-          auto jbody = storm::newStageToJSON(resp.getId());
-          return resp.staged(jbody, config);
+          auto files = storm::from_json(req.body);
+          storm::StageRequest request{files};
+          auto resp = service.stage(request);
+          return storm::to_crow_response(resp, config);
         } catch (...) {
-          return storm::TapeResponse::bad_request();
+          return storm::StageResponse::bad_request();
         }
       });
 
   CROW_ROUTE(app, "/api/v1/stage/<string>")
   ([&](std::string const& id) {
     try {
-      storm::StageRequest const* stage = service.find(id);
-      if (stage == nullptr) {
-        return storm::TapeResponse::not_found();
+      auto resp = service.status(id);
+      if (resp.stage() == nullptr) {
+        return storm::StatusResponse::not_found();
       }
-      storm::TapeResponse resp{id};
-      // TD: Decide if we have to create a TapeResponse or simply pass the
-      // id to IO operations!
-      auto jbody = storm::StagedToJSON(stage, resp.getId());
-      return resp.status(jbody);
+      return storm::to_crow_response(resp);
     } catch (...) {
-      return storm::TapeResponse::bad_request();
+      return storm::StatusResponse::bad_request();
     }
   });
 
@@ -64,108 +59,59 @@ void create_routes(crow::SimpleApp& app, storm::Configuration const& config,
       .methods("POST"_method)(
           [&](crow::request const& req, std::string const& id) {
             try {
-              storm::StageRequest* stage = service.findAndEdit(id);
-              if (stage == nullptr) {
-                return storm::TapeResponse::not_found();
+              storm::CancelRequest cancel{req.body};
+              auto resp = service.cancel(id, cancel);
+              if (resp.stage() == nullptr) {
+                return storm::CancelResponse::not_found();
               }
-              storm::Cancel cancel{req.body};
-
-              auto staged_to_cancel = service.stagedToCancel(cancel, stage);
-              // TD: These operations should be carried by service or a new
-              // "utils" object?
-              if (cancel.paths.size() != staged_to_cancel.size()) {
-                auto resp = service.checkInvalid(cancel, staged_to_cancel, id);
-                auto jbody =
-                    storm::fileMissingToJSON(resp.getInvalid(), resp.getId());
-                // TD: Decide also here if better a TapeResponse
-                // or simply pass to IO operations!
-                return resp.bad_request_with_body(jbody);
-              } else {
-                auto resp = service.cancel(cancel, stage);
-                return resp.cancelled();
+              if (resp.invalid().empty()) {
+                return storm::CancelResponse::cancelled();
               }
+              return storm::to_crow_response(resp);
             } catch (...) {
-              return storm::TapeResponse::bad_request();
+              return storm::CancelResponse::bad_request();
             }
           });
 
   CROW_ROUTE(app, "/api/v1/stage/<string>")
       .methods("DELETE"_method)([&](std::string const& id) {
         try {
-          storm::StageRequest const* stage = service.find(id);
-          if (stage == nullptr) {
-            return storm::TapeResponse::not_found();
-          }
           auto resp = service.erase(id);
-          return resp.erased();
+          if (resp.stage() == nullptr) {
+            return storm::DeleteResponse::not_found();
+          }
+          return storm::DeleteResponse::erased();
         } catch (...) {
-          return storm::TapeResponse::bad_request();
+          return storm::DeleteResponse::bad_request();
         }
       });
 
   CROW_ROUTE(app, "/api/v1/release/<string>")
-      .methods(
-          "POST"_method)([&](crow::request const& req, std::string const& id) {
-        try {
-          storm::StageRequest* stage = service.findAndEdit(id);
-          if (stage == nullptr) {
-            return storm::TapeResponse::not_found();
-          }
-          storm::Release release{req.body};
-
-          // FG what does "valid request" mean? like for cancel? just
-          // syntactically correct?
-          // TD I believe it's an existing request with a certain ID
-
-          auto staged_to_release = service.stagedToRelease(release, stage);
-
-          if (release.paths.size() != staged_to_release.size()) {
-            auto resp  = service.checkInvalid(release, staged_to_release, id);
-            auto jbody = storm::fileMissingToJSON(
-                resp.getInvalid(),
-                resp.getId()); // Decide also here if better a TapeResponse
-                               // or simply pass to IO operations!
-            return resp.bad_request_with_body(jbody);
-          }
-          // FG what does "release all provided files" mean at the
-          //"API endpoint"?
-          // TD Good question!
-          else {
-            auto resp =
-                service.release(release, stage); // TD: What do I have to do??
-            return resp.released();
-          }
-        } catch (...) {
-          return storm::TapeResponse::bad_request();
-        }
-      });
+      .methods("POST"_method)(
+          [&](crow::request const& req, std::string const& id) {
+            try {
+              storm::ReleaseRequest release{req.body};
+              auto resp = service.release(id, release);
+              if (resp.stage() == nullptr) {
+                return storm::ReleaseResponse::not_found();
+              }
+              if (resp.invalid().empty()) {
+                return storm::ReleaseResponse::released();
+              }
+              return storm::to_crow_response(resp);
+            } catch (...) {
+              return storm::ReleaseResponse::bad_request();
+            }
+          });
 
   CROW_ROUTE(app, "/api/v1/archiveinfo")
       .methods("POST"_method)([&](crow::request const& req) {
         try {
-          boost::json::array jbody;
-          storm::Archiveinfo info{req.body};
-
-          auto from_archive = service.archive();
-          jbody.reserve(info.paths.size());
-
-          auto info_from_archive = service.infoFromArchive(info, from_archive);
-
-          if (info.paths.size() != info_from_archive.size()) {
-            auto resp = service.checkInvalid(info, info_from_archive);
-            jbody     = storm::fileNotInArchiveToJSON(resp.getInvalid(), jbody);
-            auto remaining = service.compute_remaining(info, resp.getInvalid());
-            jbody          = storm::infoFromFilesToJSON(remaining, jbody);
-            return resp.fetched_from_archive(jbody);
-          } else {
-            storm::TapeResponse
-                resp; // Decide if we have to create a TapeResponse or simply
-                      // pass the id to IO operations!
-            jbody = storm::infoFromFilesToJSON(info.paths, jbody);
-            return resp.fetched_from_archive(jbody);
-          }
+          storm::ArchiveInfo info{req.body};
+          auto resp = service.archive(info);
+          return storm::to_crow_response(resp);
         } catch (...) {
-          return storm::TapeResponse::bad_request();
+          return storm::ArchiveResponse::bad_request();
         }
       });
 
