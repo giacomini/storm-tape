0d546e8a8e8047a5f3388b2733432a16c661ed7d
Tommaso Diotalevi <tommaso.diotalevi@cnaf.infn.it>
2022-09-20 16:56:07 +0000

    Update compile_src bash script

--- docker/compile_src.sh
+++ docker/compile_src.sh
@@ -2,9 +2,9 @@
 
 #Configuring build folder
 /usr/local/bin/cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE \
--DCMAKE_BUILD_TYPE:STRING=Debug -S /workspaces/storm-tape-poc \
--B /workspaces/storm-tape-poc/build -G Ninja
+-DCMAKE_BUILD_TYPE:STRING=Debug -S /storm-tape-poc \
+-B /storm-tape-poc/build -G Ninja
 
 #Building
-/usr/local/bin/cmake --build /workspaces/storm-tape-poc/build \
+/usr/local/bin/cmake --build /storm-tape-poc/build \
 --config Debug --target all
