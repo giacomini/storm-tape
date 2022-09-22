e0d6da1a7c4ba86affcdaafc6b821e1ecdb94738
Tommaso Diotalevi <tommaso.diotalevi@cnaf.infn.it>
2022-09-22 12:32:30 +0200

    Test CI with intentional mistake

--- docker/test_script.sh
+++ docker/test_script.sh
@@ -3,7 +3,7 @@
 storm-tape-poc/build/storm-tape &
 sleep 5
 response_code=`curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/.well-known/wlcg-tape-rest-apifail`
-if [$response_code != '200']
+if [$response_code != 200]
 then
     exit 1
 fi
