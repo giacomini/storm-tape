2b2665056df26ac8976dfc9b3b13c9f88062d196
Tommaso Diotalevi <tommaso.diotalevi@cnaf.infn.it>
2022-09-22 15:01:55 +0200

    Revert to normal conditions for CI

--- docker/test_script.sh
+++ docker/test_script.sh
@@ -3,7 +3,7 @@
 storm-tape-poc/build/storm-tape &
 job_id=$!
 sleep 5
-response_code=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/.well-known/wlcg-tape-rest-apifail)
+response_code=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/.well-known/wlcg-tape-rest-api)
 kill $job_id
 wait
 [ $response_code -eq 200 ]
