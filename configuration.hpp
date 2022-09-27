675a378bd33f2fbbd68f153900d792f7e5058d5b
Tommaso Diotalevi <tommaso.diotalevi@cnaf.infn.it>
2022-09-27 13:43:19 +0000

    Move well_know_uri as global variable

--- configuration.hpp
+++ configuration.hpp
@@ -3,12 +3,13 @@
 #include <string>
 
 namespace storm {
+constexpr auto c_well_known_uri{"/.well-known/wlcg-tape-rest-api"};
 struct Configuration
 {
   std::string hostname = "localhost";
   std::string base_url = "https://" + hostname + ":" + std::to_string(port);
   std::string api_uri  = base_url + "/api/v1";
-  std::string well_known_uri = "/.well-known/wlcg-tape-rest-api";
+  std::string const well_known_uri = c_well_known_uri;
   std::uint16_t port         = 8080;
 };
 } // namespace storm
