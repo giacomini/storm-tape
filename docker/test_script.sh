6604f7223061c47d61e98a4efba3068ccfb04aaf
Tommaso Diotalevi <tommaso.diotalevi@cnaf.infn.it>
2022-09-28 15:30:05 +0000

    Fix test script

--- docker/test_script.sh
+++ docker/test_script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-storm-tape-poc/build/storm-tape &
+storm-tape &
 job_id=$!
 sleep 5
 response_code=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/.well-known/wlcg-tape-rest-api)
