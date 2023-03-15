677b8d2b3b475c64ab2bd180875d15cf03057a66
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2023-03-15 15:30:35 +0100

    Add deps to debug deploy image

---
+++ docker/library-scripts/provide-deps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ $STORM_BUILD_TYPE == "debug" ]] 
+then 
+    echo "Installing depencencies for debug"
+    yum install -y centos-release-scl && 
+    yum install -y \
+        libubsan1 \
+        libasan6
+fi
