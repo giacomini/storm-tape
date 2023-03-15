41f2a25603a5660b88c26ef51ff9e3165423d97c
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2023-03-15 10:19:30 +0100

    Overhaul how dev, CI and deploy containers are built
    Adapt CI pipelines

--- .devcontainer/base-scripts/install-vcpkg.sh
+++ .devcontainer/base-scripts/install-vcpkg.sh
@@ -1,23 +1,14 @@
 #!/usr/bin/env bash
-#-------------------------------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
-#-------------------------------------------------------------------------------------------------------------
-#
+
+# Copyright 2023 Istituto Nazionale di Fisica Nucleare
+# SPDX-License-Identifier: EUPL-1.2
+
+# Adapted from https://github.com/devcontainers/images/blob/main/src/cpp/.devcontainer/base-scripts/install-vcpkg.sh
+
+
 set -e
 
 USERNAME=${1:-"vscode"}
-
-# bionic and stretch pkg repos install cmake version < 3.15 which is required to run bootstrap-vcpkg.sh on ARM64 
-VCPKG_UNSUPPORTED_ARM64_VERSION_CODENAMES="stretch bionic"
-
-. /etc/os-release
-
-# Exit early if ARM64 OS does not have cmake version required to build Vcpkg
-if [ "$(dpkg --print-architecture)" = "arm64" ] && [[ "${VCPKG_UNSUPPORTED_ARM64_VERSION_CODENAMES}" = *"${VERSION_CODENAME}"* ]]; then
-    echo "OS ${VERSION_CODENAME} ARM64 pkg repo installs cmake version < 3.15, which is required to build Vcpkg."
-    exit 0
-fi
 
 # Add to bashrc/zshrc files for all users.
 updaterc()  {
@@ -30,29 +21,6 @@ updaterc()  {
     fi
 }
 
-# Run apt-get if needed.
-apt_get_update_if_needed() {
-    if [ ! -d "/var/lib/apt/lists" ] || [ "$(ls /var/lib/apt/lists/ | wc -l)" = "0" ]; then
-        echo "Running apt-get update..."
-        apt-get update
-    else
-        echo "Skipping apt-get update."
-    fi
-}
-
-# Check if packages are installed and installs them if not.
-check_packages() {
-    if ! dpkg -s "$@" > /dev/null 2>&1; then
-        apt_get_update_if_needed
-        apt-get -y install --no-install-recommends "$@"
-    fi
-}
-
-export DEBIAN_FRONTEND=noninteractive
-
-# Install additional packages needed by vcpkg: https://github.com/microsoft/vcpkg/blob/master/README.md#installing-linux-developer-tools
-check_packages build-essential tar curl zip unzip pkg-config bash-completion ninja-build git
-
 # Setup group and add user
 umask 0002
 if ! cat /etc/group | grep -e "^vcpkg:" > /dev/null 2>&1; then
@@ -64,18 +32,15 @@ usermod -a -G "vcpkg" "${USERNAME}"
 # Clone repository with ports and installer
 mkdir -p "${VCPKG_ROOT}"
 mkdir -p "${VCPKG_DOWNLOADS}"
-git clone --depth=1 \
+git clone \
     -c core.eol=lf \
     -c core.autocrlf=false \
     -c fsck.zeroPaddedFilemode=ignore \
     -c fetch.fsck.zeroPaddedFilemode=ignore \
     -c receive.fsck.zeroPaddedFilemode=ignore \
     https://github.com/microsoft/vcpkg "${VCPKG_ROOT}"
-##    -b stable_tommaso --single-branch \
-##    https://github.com/Tommaso93/vcpkg "${VCPKG_ROOT}"
 
 ## Run installer to get latest stable vcpkg binary
-## https://github.com/microsoft/vcpkg/blob/7e7dad5fe20cdc085731343e0e197a7ae655555b/scripts/bootstrap.sh#L126-L144
 "${VCPKG_ROOT}"/bootstrap-vcpkg.sh
 
 # Add vcpkg to PATH
@@ -89,7 +54,3 @@ EOF
 chown -R ":vcpkg" "${VCPKG_ROOT}" "${VCPKG_DOWNLOADS}"
 chmod g+r+w+s "${VCPKG_ROOT}" "${VCPKG_DOWNLOADS}"
 chmod -R g+r+w "${VCPKG_ROOT}" "${VCPKG_DOWNLOADS}"
-
-# Enable tab completion for bash and zsh
-VCPKG_FORCE_SYSTEM_BINARIES=1 su "${USERNAME}" -c "${VCPKG_ROOT}/vcpkg integrate bash"
-VCPKG_FORCE_SYSTEM_BINARIES=1 su "${USERNAME}" -c "${VCPKG_ROOT}/vcpkg integrate zsh"
