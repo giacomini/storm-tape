c58145154938387d90d9f3170648d5779a6a98f7
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2023-03-15 17:56:45 +0100

    Fix missing backslash

--- docker/library-scripts/provide-deps.sh
+++ docker/library-scripts/provide-deps.sh
@@ -5,7 +5,7 @@ STORM_BUILD_TYPE=${1}
 if [[ $STORM_BUILD_TYPE == "debug" ]] 
 then 
     echo "Installing depencencies for debug"
-    yum install -y centos-release-scl && 
+    yum install -y centos-release-scl
     yum install -y \
         libubsan1 \
         libasan6
