d37404e8febe991c9e5b6cea08c75795e52843f1
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2023-03-24 10:37:00 +0000

    Fix failing ci-test stage
    Changed curl to .well-know to favicon.ico.
    Remove always success result to test stage

--- test/test_script.sh
+++ test/test_script.sh
@@ -3,7 +3,7 @@
 storm-tape &
 job_id=$!
 sleep 5
-response_code=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/.well-known/wlcg-tape-rest-api)
+response_code=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/favicon.ico)
 kill $job_id
 wait
-[ $response_code -eq 200 ]
+[ $response_code -eq 204 ]
