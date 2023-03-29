d0353d7ce4e3c00d0b35946d7a52d0d3aad64a4c
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2023-03-29 10:13:03 +0200

    Add double quote to provent globbing and word splitting

--- .devcontainer/library-scripts/provide-cmake.sh
+++ .devcontainer/library-scripts/provide-cmake.sh
@@ -2,11 +2,10 @@
 
 # Copyright 2023 Istituto Nazionale di Fisica Nucleare
 # SPDX-License-Identifier: EUPL-1.2
-
 set -ex
 
-wget https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-${PLATFORM}-${ARCH}.sh && \
-    chmod +x cmake-*.sh && \
-    ./cmake-*.sh --prefix=/usr/local --skip-license && \
+wget "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-${PLATFORM}-${ARCH}.sh" && \
+    chmod +x "cmake-${CMAKE_VERSION}-${PLATFORM}-${ARCH}".sh && \
+    ./"cmake-${CMAKE_VERSION}-${PLATFORM}-${ARCH}.sh" --prefix=/usr/local --skip-license && \
     rm -rf cmake-* && \
     cmake --version
