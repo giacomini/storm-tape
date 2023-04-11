1df2b5ceae42d9f8cf83c21b1284a24ade7c916b
Francesco Giacomini <francesco.giacomini@cnaf.infn.it>
2023-04-11 12:37:19 +0200

    Don't consider files being recalled as on disk
    Moreover, if reading an xattr fails, the locality is set to
    unavailable.
    
    Resolves: #2

--- src/local_storage.cpp
+++ src/local_storage.cpp
@@ -18,16 +18,33 @@ Locality LocalStorage::locality(Path const& physical_path)
 
   constexpr auto bytes_per_block{512L};
   bool const is_stub{sb.st_blocks * bytes_per_block < sb.st_size};
+
+  std::error_code ec;
+
+  bool const is_being_recalled{[&] {
+    XAttrName const tsm_rect{"user.TSMRecT"};
+    return has_xattr(physical_path, tsm_rect, ec);
+  }()};
+
+  if (ec != std::error_code{}) {
+    return Locality::unavailable;
+  }
+
+  bool const is_on_disk{!(is_stub || is_being_recalled)};
+
   bool const is_on_tape{[&] {
     XAttrName const storm_migrated{"user.storm.migrated"};
-    std::error_code ec;
     return has_xattr(physical_path, storm_migrated, ec);
   }()};
 
-  if (is_stub) {
-    return is_on_tape ? Locality::tape : Locality::lost;
-  } else {
+  if (ec != std::error_code{}) {
+    return Locality::unavailable;
+  }
+
+  if (is_on_disk) {
     return is_on_tape ? Locality::disk_and_tape : Locality::disk;
+  } else {
+    return is_on_tape ? Locality::tape : Locality::lost;
   }
 }
 
