f7809ccff4fdc8331211cb682c55ee716e9de448
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2023-07-03 14:37:18 +0200

    Add storm::Exception and storm::BadRequest exception

--- src/errors.hpp
+++ src/errors.hpp
@@ -2,11 +2,31 @@
 #define ERRORS_HPP
 
 #define BOOST_ENABLE_ASSERT_HANDLER
+#include "types.hpp"
 #include <boost/assert.hpp>
 
 namespace boost {
 void assertion_failed(char const* expr, char const* function, char const* file,
                       long line);
 }
+
+namespace storm {
+
+class Exception : public std::runtime_error
+{
+  public:
+    using std::runtime_error::runtime_error;
+    virtual int http_code() const     = 0;
+};
+
+class BadRequest : public Exception {
+  public:
+  explicit BadRequest(std::string title)
+    : Exception(std::move(title))
+    {}
+
+  int http_code() const override { return 400; }
+};
+} // namespace storm
 
 #endif
