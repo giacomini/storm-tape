8fbd60533f07929cf9155247306b4da588a9c89d
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2023-10-18 14:36:53 +0000

    Add LogicalPath and PhysicalPath structs and vector aliases

--- src/types.hpp
+++ src/types.hpp
@@ -8,10 +8,14 @@ namespace storm {
 
 namespace fs = std::filesystem;
 
-using Path      = fs::path;
-using Paths     = std::vector<Path>;
-using TimePoint = long long int;
-using StageId   = std::string;
+struct LogicalPath;
+struct PhysicalPath;
+using Path          = fs::path;
+using Paths         = std::vector<Path>;
+using LogicalPaths  = std::vector<LogicalPath>;
+using PhysicalPaths = std::vector<PhysicalPath>;
+using TimePoint     = long long int;
+using StageId       = std::string;
 
 enum class Locality : unsigned char
 {
@@ -21,6 +25,16 @@ enum class Locality : unsigned char
   disk_and_tape,
   lost,
   none
+};
+
+struct LogicalPath : fs::path
+{
+  using fs::path::path;
+};
+
+struct PhysicalPath : fs::path
+{
+  using fs::path::path;
 };
 
 std::string to_string(Locality locality);
